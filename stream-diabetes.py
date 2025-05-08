7b33551143e74e83a8d6c4ac170eb3dc37d94f06
Raihan Nur Rizqillah <147234583+gingerlyi@users.noreply.github.com>
2025-05-09 03:10:51 +0800

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -29,10 +29,10 @@ diabetes_diagnosis = ''
 if st.button('Test Prediksi') :
      diab_prediction = diabetes_model.predict([[Pregnancies, Glucose ,BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
-if (diab_prediction[0] == 1):
-     diab_diagnosis = 'Pasien Terdiagnosa Diabetes'
-else :
-     diab_diagnosis = 'Pasien Tidak Terdiagnosa Diabetes'
+     if (diab_prediction[0] == 1):
+          diab_diagnosis = 'Pasien Terdiagnosa Diabetes'
+     else :
+          diab_diagnosis = 'Pasien Tidak Terdiagnosa Diabetes'
 
 st.success(diab_diagnosis)
 
